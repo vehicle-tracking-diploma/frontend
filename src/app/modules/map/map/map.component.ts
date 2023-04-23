d4589d139817468e53f9b7a74d94b0b18ad65924
zhannaorazkan <muratkzhanna@gmail.com>
2023-04-23 14:59:53 +0600

    added: leaflet

--- src/app/modules/map/map/map.component.ts
+++ src/app/modules/map/map/map.component.ts
@@ -1,36 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import Map from 'ol/Map'
 import {View} from "ol";
 import TileLayer from "ol/layer/Tile";
 import {OSM} from "ol/source";
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
   // @ts-ignore
-  map:Map;
+  private map;
+
+  private initMap(): void {
+
+    this.map = L.map('map', {
+
+      center: [ 39.8282, -98.5795 ],
+
+      zoom: 3
+
+    });
+
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+      maxZoom: 18,
+
+      minZoom: 3,
+
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+    });
+
+    tiles.addTo(this.map);
+
+  }
 
   constructor() { }
 
-
-  ngOnInit() {
-    // @ts-ignore
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 2,
-      }),
-    });
+  ngAfterViewInit(): void {
+    this.initMap();
   }
+
+
+
 }
 
